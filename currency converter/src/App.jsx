f9846b8177db6de7a41b5c723c00fe1f4112e212
michaelharrison11 <your.email@example.com>
2025-10-06 16:19:47 -0700

    start

--- currency converter/src/App.jsx
+++ currency converter/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./styles.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [currencies, setCurrencies] = useState([]);
+
+  useEffect(() => {
+    const url =
+      "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+    fetch(url)
+      .then((r) => r.json())
+      .then((response) => {
+        const entries = Object.entries(response);
+        setCurrencies(entries); 
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Currencies</h1>
+      <table>
+        <tbody>
+          <tr>
+            <th>Code</th>
+            <th>Name</th>
+          </tr>
+          {currencies.map(([code, name]) => (
+            <tr key={code}>
+              <td>{code.toUpperCase()}</td>
+              <td>{name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
-
-export default App
